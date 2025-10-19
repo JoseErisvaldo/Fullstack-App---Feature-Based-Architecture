f822b4fe0a56efb6a87f1f0243db9ee29ed10d48
joseerisvaldopg <joseerisvaldopg@gmail.com>
2025-10-19 00:32:22 -0300

    initial structure

---
+++ frontend/src/modules/users/pages/UsersPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UsersPage = () => {
+    return (
+        <div className='p-8'>
+            <h1 className='text-2xl font-bold mb-4'>Users</h1>
+            <p>Lista de usuários aqui...</p>
+        </div>
+    );
+};
+
+export default UsersPage;
